a1e9707912be8bbdfbb22df6bee66246b471b1c7
JesusSaavedra7 <angel.benavidesdr@uanl.edu.mx>
2023-10-25 16:41:47 -0600

    lete

--- JesusSaavedra/Js/apps.js
+++ JesusSaavedra/Js/apps.js
@@ -93,25 +93,28 @@ const app4SoloPunto = document.getElementById("app4SoloPunto");
 const app4Res = document.getElementById("app4Res");
 const app4BtnVer = document.getElementById("app4BtnVer");
 
-
+    /*
     app4BtnVer.addEventListener("click", (e) => {
-    e.preventDefault();
+    e.preventDefault();*/
 
+
+    function mostrarTabla(){
     let tabla = parseInt(app4Tabla.value);
     let lista = document.createElement("ul");
 
     if( !app4Inv.checked ){
         for (let i=1; i <= 10; i++){
             
-            //let resMult =app4SoloPunto.checked ? ".".repeat(tabla * i) : resMult = tabla * i;
+            let resMult =app4SoloPunto.checked ? ".".repeat(tabla * i) : tabla * i;
 
-            let resMult;
+            /*let resMult;
             if(app4SoloPunto.checked){
                 resMult = ".".repeat(tabla * i)
             }
             else{
                 resMult = tabla * i;
             }
+            */
 
             let mult = tabla + "X" + i +"="+ resMult;
             let item = document.createElement("li");
@@ -123,15 +126,15 @@ const app4BtnVer = document.getElementById("app4BtnVer");
     else {
         for (let i=10; i >= 1; i--){
 
-            
-            let resMult;
+            let resMult= app4SoloPunto.checked ? ".".repeat(tabla*i) : tabla*i;
+            /*let resMult;
             if(app4SoloPunto.checked){
                 resMult = ".".repeat(tabla * i)
             }
             else{
                 resMult = tabla * i;
             }
-
+            */
             let mult = tabla + "X" + i +"="+ resMult;
             let item = document.createElement("li");
             item.innerHTML=mult;
@@ -143,16 +146,19 @@ const app4BtnVer = document.getElementById("app4BtnVer");
     app4Res.appendChild(lista);
 
     
+}
+
+app4BtnVer.addEventListener("click", (e) =>{
+    e.preventDefault();
+    mostrarTabla()
 });
 
-app4Inv.addEventListener("change", (e) => {
-    
+app4Inv.addEventListener("change", (e) =>{
+    mostrarTabla()
 });
-
-app4SoloPunto.addEventListener("change", (e)  => {
-    
+app4SoloPunto.addEventListener("change", (e) =>{
+    mostrarTabla()
 });
-
 
 
 

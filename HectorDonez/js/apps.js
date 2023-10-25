1c6a809e3877ff1e22c512016723f3677a8ad9a3
Héctor Eduardo Colorado Doñez <hecorcolorado16@gmial.com>
2023-10-24 21:43:07 -0600

    clase 04 oct

--- HectorDonez/js/apps.js
+++ HectorDonez/js/apps.js
@@ -32,3 +32,82 @@ app2BtnRepetir.addEventListener("click", (e) => {
 
     app2Res.value = resultado;
 });
+
+
+
+/* App3 */
+const app3Num1 = document.getElementById("app3Num1");
+const app3Num2 = document.getElementById("app3Num2");
+const app30operacion = document.getElementById("app30operacion");
+const app3Res = document.getElementById("app3Res");
+const app3BtnCalcular = document.getElementById("app3BtnCalcular");
+
+app3BtnCalcular.addEventListener("click",(e) => {
+    e.preventDefault();
+    let n1 = parseInt(app3Num1.value);
+    let n2 = parseInt(app3Num2.value);
+
+    let operacion = app30operacion.value;
+    let resultado;
+
+    /* Checar que operacion selecciono el usuario*/
+    /*if(operacion == "s"){
+        resultado = n1 + n2;
+    }
+    else if(operacion == "r"){
+        resultado = n1 - n2;
+    }
+    else if(operacion == "m"){
+        resultado = n1 * n2;
+    }
+    else if(operacion == "d"){
+        resultado = n1 / n2;
+    }
+    */
+    switch(operacion){
+        case "s": resultado = n1 + n2;
+                    break;
+        case "r": resultado = n1 - n2;
+                    break;
+        case "m": resultado = n1 * n2;
+                    break;
+        case "d": resultado = n1 / n2;
+                    break;            
+    }
+    app3Res.value = resultado;
+});
+
+/* App 4*/
+const app4Tabla = document.getElementById("app4Tabla");
+const app4Inv = document.getElementById("app4Inv");
+const app4SoloPunto = document.getElementById("appSoloPunto");
+const app4Res = document.getElementById("app4Res");
+const appBtnVer = document.getElementById("app4BtnVer");
+
+appBtnVer.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let tabla = parseInt(app4Tabla.value);
+
+    let lista = document.createElement("ul");
+
+    if(!app4Inv.checked ){
+        for(let i=1; i <= 10 ; i++ ){
+            let mult = tabla + " X " + i + " = " + tabla * i;
+            let item = document.createElement("li");
+            item.innerHTML = mult;
+            lista.appendChild(item);
+        }
+    }
+    else{
+        for(let i=10; i >= 1 ; i-- ){
+            let mult = tabla + " X " + i + " = " + tabla * i;
+            let item = document.createElement("li");
+            item.innerHTML = mult;
+            lista.appendChild(item);
+        }
+    }
+
+    app4Res.innerHTML = "";
+    app4Res.appendChild(lista);
+});

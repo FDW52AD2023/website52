a0c2ea1321bcf18054190c846d683073c7d984de
andrea melina santos garcia <melina.santosgrc@uanl.edu.mx>
2023-10-26 14:33:15 -0600

    clase23octterminado

--- melinasantos0404/JS/clase23oct.js
+++ melinasantos0404/JS/clase23oct.js
@@ -1,57 +1,37 @@
-for (let cont=1 ; cont <=10 ; cont ++ ){
-console.log(cont);
-}
+//App 3//
 
-
-
-/*apps3*/
 const app3Num1 = document.getElementById("app3Num1");
 const app3Num2 = document.getElementById("app3Num2");
-const app3operaciones = document.getElementById("app3operaciones");
-const app3Res = document.getElementById ("app3Res");
+const app3Operacion = document.getElementById("app3Operacion");
+const app3Res = document.getElementById("app3Res");
 const app3BtnCalcular = document.getElementById("app3BtnCalcular");
 
-
-app3Btncalcular.addEventlistener("click",(e) =>{
+app3BtnCalcular.addEventListener("click",(e) =>{
     e.preventDefault();
 
-    let n1 =parseInt(app3Num1.value)
-    let n2 =parseInt(app3Num2.value)
-    let operacion = app3operaciones.value;
+    let n1 = parseInt(app3Num1.value);
+    let n2 = parseInt(app3Num2.value);
+    let operacion = app3Operacion.value;
     let resultado;
-    
-    /*checar que operacion selecciono el usuario*/
-    /*if( operacion == "s") {
-       resultado = n1 + n2
-    }
-    
-    else if (operacion == "r"){
-       resultado =n1 -n2;
-    
-    }
-    
-    else if (operacion== "m"){
-       resultado = n1 * n2;
-    
-    }
-    
-    else if (operacion == "d" ){
-       resultado = n1 / n2;
-    
-    }*/
-})
-switch(operaciones){
-   case "s": resultado =n1 + n2;
-   break;
-   case "r": resultado =n1 - n2;
-   break;
-   case "m": resultado =n1 * n2;
-   break;
-   case "d": resultado =n1 / n2;
-   break;
-}
 
-//APP 4:)//
+    switch(operacion){
+        case "s" : resultado = n1 + n2;
+                    break;
+
+        case "r" : resultado = n1 - n2;
+                    break;
+
+        case "m" : resultado = n1 * n2;
+                    break;
+
+        case "d" : resultado = n1 / n2;
+                    break;
+    }
+
+    app3Res.value =resultado;
+});
+
+//APP 4//
 const app4Tabla = document.getElementById("app4Tabla");
 const app4Inv = document.getElementById("app4Inv");
 const app4SoloPunto = document.getElementById("app4SoloPunto");
@@ -68,6 +48,8 @@ const app4BtnVer = document.getElementById("app4BtnVer");
             
             let resMult =app4SoloPunto.checked ? ".".repeat(tabla * i) : tabla * i;
 
+           
+
             let mult = tabla + "X" + i +"="+ resMult;
             let item = document.createElement("li");
             item.innerHTML=mult;
@@ -79,7 +61,7 @@ const app4BtnVer = document.getElementById("app4BtnVer");
         for (let i=10; i >= 1; i--){
 
             let resMult= app4SoloPunto.checked ? ".".repeat(tabla*i) : tabla*i;
-        
+            
             let mult = tabla + "X" + i +"="+ resMult;
             let item = document.createElement("li");
             item.innerHTML=mult;
@@ -104,3 +86,48 @@ app4Inv.addEventListener("change", (e) =>{
 app4SoloPunto.addEventListener("change", (e) =>{
     mostrarTabla()
 });
+
+
+//App 5//
+const app5Color = document.getElementById("app5Color");
+const app5Borde = document.getElementById("app5Borde");
+const app5Grosor = document.getElementById("app5Grosor");
+const app5Tipo = document.getElementById("app5Tipo");
+const resultado5 = document.getElementById("resultado5");
+const app5BtnVer = document.getElementById("app5BtnVer");
+
+function actualizarborde() {
+    const posicion = app5Borde.value;
+    const grosor = app5Grosor.value;
+    const tipo = app5Tipo.value;
+    const borde = `${grosor} ${tipo} black`;
+
+    resultado5.style.border = 'none';
+    resultado5.style.borderTop = 'none';
+    resultado5.style.borderRight = 'none';
+    resultado5.style.borderBottom = 'none';
+    resultado5.style.borderLeft = 'none';
+
+
+    if (posicion === 'all') {
+        resultado5.style.border = borde;
+        
+    } else {
+        resultado5.style['border' + posicion.charAt(0).toUpperCase() + posicion.slice(1)] = borde;
+    }
+}
+
+app5BtnVer.addEventListener("click", (e) =>{
+    e.preventDefault();
+    actualizarborde()
+});
+app5BtnVer.addEventListener("click" , (e) => {
+    actualizarborde()
+});
+
+app5BtnVer.addEventListener("click", (e) => {
+    const colorElegido = app5Color.value;
+       
+        resultado5.style.backgroundColor = colorElegido;
+});
+

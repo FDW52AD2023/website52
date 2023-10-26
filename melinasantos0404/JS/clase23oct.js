b39468593bfd0db075838cf4bf677a98b2864d58
andrea melina santos garcia <melina.santosgrc@uanl.edu.mx>
2023-10-26 14:29:49 -0600

    idk

--- melinasantos0404/JS/clase23oct.js
+++ melinasantos0404/JS/clase23oct.js
@@ -1,10 +1,5 @@
-for (let cont=1 ; cont <=10 ; cont ++ ){
-console.log(cont);
-}
 
-
-
-/* App3 */
+//App 3//
 
 const app3Num1 = document.getElementById("app3Num1");
 const app3Num2 = document.getElementById("app3Num2");
@@ -12,7 +7,7 @@ const app3Operacion = document.getElementById("app3Operacion");
 const app3Res = document.getElementById("app3Res");
 const app3BtnCalcular = document.getElementById("app3BtnCalcular");
 
-app3BtnCalcular.addEventListener("click",(e) => {
+app3BtnCalcular.addEventListener("click",(e) =>{
     e.preventDefault();
 
     let n1 = parseInt(app3Num1.value);
@@ -20,34 +15,22 @@ app3BtnCalcular.addEventListener("click",(e) => {
     let operacion = app3Operacion.value;
     let resultado;
 
-    /*que operacion selecciono el usuario */
-    /* if( operacion == "s" ){
-        resultado = n1 + n2;
-    }
-    else if( operacion == "r"){
-        resultado = n1 -n2;
-    }
-    else if( operacion == "m"){
-        resultado = n1 * n2;
-    }
-    else if ( operacion == "d"){
-        resultado = n1 / n2;
-    } */
     switch(operacion){
-        case "s": resultado = n1 + n2;
-                break;
-        case "r": resultado = n1 - n2;
+        case "s" : resultado = n1 + n2;
                     break;
+
+        case "r" : resultado = n1 - n2;
+                    break;
+
         case "m" : resultado = n1 * n2;
                     break;
+
         case "d" : resultado = n1 / n2;
                     break;
     }
 
-    app3Res.value = resultado;
+    app3Res.value =resultado;
 });
-
-
 
 //APP 4//
 const app4Tabla = document.getElementById("app4Tabla");
@@ -55,6 +38,7 @@ const app4Inv = document.getElementById("app4Inv");
 const app4SoloPunto = document.getElementById("app4SoloPunto");
 const app4Res = document.getElementById("app4Res");
 const app4BtnVer = document.getElementById("app4BtnVer");
+
 
     function mostrarTabla(){
     let tabla = parseInt(app4Tabla.value);
@@ -64,6 +48,8 @@ const app4BtnVer = document.getElementById("app4BtnVer");
         for (let i=1; i <= 10; i++){
             
             let resMult =app4SoloPunto.checked ? ".".repeat(tabla * i) : tabla * i;
+
+           
 
             let mult = tabla + "X" + i +"="+ resMult;
             let item = document.createElement("li");
@@ -76,6 +62,7 @@ const app4BtnVer = document.getElementById("app4BtnVer");
         for (let i=10; i >= 1; i--){
 
             let resMult= app4SoloPunto.checked ? ".".repeat(tabla*i) : tabla*i;
+            
             let mult = tabla + "X" + i +"="+ resMult;
             let item = document.createElement("li");
             item.innerHTML=mult;
@@ -84,7 +71,9 @@ const app4BtnVer = document.getElementById("app4BtnVer");
 
     }
     app4Res.innerHTML="";
-    app4Res.appendChild(lista);  
+    app4Res.appendChild(lista);
+
+    
 }
 
 app4BtnVer.addEventListener("click", (e) =>{
@@ -114,7 +103,6 @@ function actualizarborde() {
     const tipo = app5Tipo.value;
     const borde = `${grosor} ${tipo} black`;
 
-    // Reiniciar las propiedades de borde antes de aplicar el nuevo estilo//
     resultado5.style.border = 'none';
     resultado5.style.borderTop = 'none';
     resultado5.style.borderRight = 'none';
@@ -124,7 +112,7 @@ function actualizarborde() {
 
     if (posicion === 'all') {
         resultado5.style.border = borde;
-         // Construye el nombre de la propiedad de estilo de borde de manera dinámica//
+        
     } else {
         resultado5.style['border' + posicion.charAt(0).toUpperCase() + posicion.slice(1)] = borde;
     }
@@ -140,64 +128,6 @@ app5BtnVer.addEventListener("click" , (e) => {
 
 app5BtnVer.addEventListener("click", (e) => {
     const colorElegido = app5Color.value;
-        // Actualiza el color de fondo del rectángulo para reflejar la selección del usuario
+       
         resultado5.style.backgroundColor = colorElegido;
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

c28d39e1147bd22f020463953480c7b184611ba5
andrea melina santos garcia <melina.santosgrc@uanl.edu.mx>
2023-10-26 14:17:04 -0600

    errores

--- melinasantos0404/JS/clase23oct.js
+++ melinasantos0404/JS/clase23oct.js
@@ -51,45 +51,56 @@ switch(operaciones){
    break;
 }
 
+//APP 4:)//
+const app4Tabla = document.getElementById("app4Tabla");
+const app4Inv = document.getElementById("app4Inv");
+const app4SoloPunto = document.getElementById("app4SoloPunto");
+const app4Res = document.getElementById("app4Res");
+const app4BtnVer = document.getElementById("app4BtnVer");
 
 
-/*APP 4 */
-const app4Tabla = document.getElementById ("app4Tabla");
-const app4Inv = document.getElementById ("app4INv");
-const app4SoloPunto = document.getElementById ("app4SoloPunto");
-const app4Res = document.getElementById ("app4Res");
-const app4BtnVer = document.getElementById ("app4BtnVer");
+    function mostrarTabla(){
+    let tabla = parseInt(app4Tabla.value);
+    let lista = document.createElement("ul");
 
-app4BtnVer. addEventListener ("click", (e) => {
-    e.preventDefault();
-    let  tabla = parseInt( app4Tabla.value);
-let lista = document.createElement("ul");
- 
+    if( !app4Inv.checked ){
+        for (let i=1; i <= 10; i++){
+            
+            let resMult =app4SoloPunto.checked ? ".".repeat(tabla * i) : tabla * i;
 
+            let mult = tabla + "X" + i +"="+ resMult;
+            let item = document.createElement("li");
+            item.innerHTML=mult;
+            lista.appendChild(item);
+        }
 
-if(app4Inv == false ) {
-    for (let i=1; i <= 10; i++){
-        let mult = tabla + "X" + i + " = "+ tabla * i; 
-        let item =document.createElement("li")
-        item.innerHTML =mult;
-         lista.appendChild(item);
-       
     }
+    else {
+        for (let i=10; i >= 1; i--){
 
+            let resMult= app4SoloPunto.checked ? ".".repeat(tabla*i) : tabla*i;
+        
+            let mult = tabla + "X" + i +"="+ resMult;
+            let item = document.createElement("li");
+            item.innerHTML=mult;
+            lista.appendChild(item);
+        }
+
+    }
+    app4Res.innerHTML="";
+    app4Res.appendChild(lista);
+
+    
 }
-else {
-    for (let i=1; i <= 10; i++){
-        let mult = tabla + "X" + i + " = "+ tabla * i; 
-        let item =document.createElement("li")
-        item.innerHTML =mult;
-         lista.appendChild(item); 
-       
-}
-}
 
-app4Res.innerHTML = "";
-app4Res.appendChild(lista);
+app4BtnVer.addEventListener("click", (e) =>{
+    e.preventDefault();
+    mostrarTabla()
+});
 
-
-   
-})
+app4Inv.addEventListener("change", (e) =>{
+    mostrarTabla()
+});
+app4SoloPunto.addEventListener("change", (e) =>{
+    mostrarTabla()
+});
